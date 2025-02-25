604f2df9c675f1b7804e759029a1dc10ddf5d4a8
wormythegreat <lukealford06@gmail.com>
2025-02-25 21:41:03 +0000

    First go at plotting nullclides

--- Question_4.py
+++ Question_4.py
@@ -1,1 +1,49 @@
 #Question 4
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Set up plot
+plt.axhline(0,0,1,color="grey")
+plt.axvline(0,0,1,color="grey")
+plt.xlabel("Red Squirrels (r)")
+plt.ylabel("Grey Squirrels (g)")
+plt.title("Nullclines of r and g")
+
+#Nullclines
+
+#for r_dot = 0
+#g=1-r
+
+def null_r(r):
+    g = 1 - r
+    return g
+
+#for g_dot = o
+#r=2-3g
+
+def null_g(g):
+    r = 2 - (3 * g)
+    return r
+
+#Plotting nuclides with r as x-axis and g as y-axis
+
+Nr = np.linspace(-5,5,500)
+Ng = []
+index = 0
+for count in Nr:
+    Ng.append(null_r(Nr[index]))
+    index = index + 1
+
+plt.plot(Nr,Ng,'r')
+
+Nr = np.linspace(-5,5,500)
+Ng = []
+index = 0
+for count in Nr:
+    Ng.append(null_g(Nr[index]))
+    index = index + 1
+
+plt.plot(Nr,Ng,'b')
+
+plt.savefig("./Nullclines.png")
