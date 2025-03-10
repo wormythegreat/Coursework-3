e6255201c7c6fe8ab2f87d6ff703ce4292b676c5
wormythegreat <lukealford06@gmail.com>
2025-03-10 14:06:55 +0000

    Added quiver for q3

--- Question_3_Quiver.py
+++ Question_3_Quiver.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-R = np.linspace(0, 1.3, 30)
-G = np.linspace(0, 0.8, 30)
+R = np.linspace(0, 3, 30)
+G = np.linspace(0, 2, 30)
 r, g = np.meshgrid(R,G)
-F = r * (1 - r - g)
-H = g * (2 - r - (3 * g))
+F = r * (1 - g)
+H = g * (2 - r)
 fig, ax = plt.subplots()
 ax.quiver(r, g, F, H)
 plt.xlabel("Red Squirrels (r)")
 plt.ylabel("Grey Squirrels (g)")
-plt.title("(Q4) Quiver Plot of r and g")
-plt.savefig("./Quiver.png")
+plt.title("(Q3) Quiver Plot of r and g")
+plt.savefig("./Quiver(Q4).png")
