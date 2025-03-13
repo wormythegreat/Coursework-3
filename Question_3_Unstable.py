3063013fc4bf4b0e47348ece46ed61d030357b12
wormythegreat <lukealford06@gmail.com>
2025-03-13 17:30:45 +0000

    Does not work

---
+++ Question_3_Unstable.py
@@ -0,0 +1,30 @@
+from Question_2 import rdot,gdot
+import matplotlib.pyplot as plt
+import numpy as np
+from math import floor
+
+plt.clf()
+plt.axhline(0,0,1,color="grey")
+plt.axvline(0,0,1,color="grey")
+plt.xlabel("Red Squirrels (r)")
+plt.ylabel("Grey Squirrels (g)")
+plt.title("Unstable Manifold")
+
+def plot_line(start_r,start_g):
+    max = 6
+    t = 0.1
+    nstep = floor(max/t)
+    t_total = [0]
+    r = [start_r]   #make this the saddle point its self
+    g = [start_g]
+
+    for j in range(nstep):
+        r.append(r[j] + (t * rdot(r[j],g[j])))
+        g.append(g[j] + (t * gdot(r[j],g[j])))
+        t_total.append(float(t_total[j] + t))
+
+    plt.plot(r,g,'orange')
+
+plot_line(2.1,1.1)
+
+plt.show()
