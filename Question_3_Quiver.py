53961aa1e4a00c504cd1df30424f5d0b77d9f1d9
wormythegreat <lukealford06@gmail.com>
2025-03-18 17:40:05 +0000

    Added comments for q2 quiver

--- Question_3_Quiver.py
+++ Question_3_Quiver.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Sets up equally spaced points to draw the quiver arrows from
 R = np.linspace(0, 3, 30)
 G = np.linspace(0, 2, 30)
 r, g = np.meshgrid(R,G)
+#These are the differential equations
 F = r * (1 - g)
 H = g * (2 - r)
 fig, ax = plt.subplots()
 ax.quiver(r, g, F, H)
+#Axes, axes titles and plot titles
 plt.xlabel("Red Squirrels (r)")
 plt.ylabel("Grey Squirrels (g)")
 plt.axhline(0,0,1,color="blue")
